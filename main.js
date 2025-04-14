c394ec7f9cdd554d5c9fce7c85e72335dd21db34
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-04-14 17:27:19 +0200

    finito

--- main.js
+++ main.js
@@ -24,13 +24,10 @@ let footballTeams = [
 console.log(footballTeams);
 
 function randomNumber() {
-  const number = Math.floor(Math.random() * 5) + 1;
-  for (let i = 0; i < footballTeams.length; i++) {
-    currentNumber = number;
-    footballTeams[0].puntiFatti = currentNumber;
-    console.log(number);
-    console.log(footballTeams[0].puntiFatti);
-    return number;
-  }
+  footballTeams.forEach((team) => {
+    team.puntiFatti = Math.floor(Math.random() * 10) + 1;
+    team.falliSubiti = Math.floor(Math.random() * 10) + 1;
+  });
 }
+
 randomNumber();
