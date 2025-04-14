be89a7f5e68338d6c93e80406305ad15b1ea21f9
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-04-14 17:11:57 +0200

    prova

--- main.js
+++ main.js
@@ -6,10 +6,31 @@ const bikes = [
 ];
 
 let lighterBike = bikes[0];
-for (let i = 1; i < bikes.length; i++) {
+for (let i = 0; i < bikes.length; i++) {
   if (bikes[i].peso < lighterBike.peso) {
     lighterBike = bikes[i];
   }
 }
 
 console.log(lighterBike);
+
+// #### SNACK 2 ####
+let footballTeams = [
+  { nome: "Roma", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Lazio", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Inter", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Milan", puntiFatti: 0, falliSubiti: 0 },
+];
+console.log(footballTeams);
+
+function randomNumber() {
+  const number = Math.floor(Math.random() * 5) + 1;
+  for (let i = 0; i < footballTeams.length; i++) {
+    currentNumber = number;
+    footballTeams[0].puntiFatti = currentNumber;
+    console.log(number);
+    console.log(footballTeams[0].puntiFatti);
+    return number;
+  }
+}
+randomNumber();
